4e8f08790b15fe52e7d0056b9cce39c941a36227
Emeti <emetisuccess@gmail.com>
2022-09-12 19:15:29 +0100

    committed

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import { HiMenuAlt4 } from "react-icons/hi";
+import { AiOutlineClose } from "react-icons/ai";
+import logo from "../../images/logo.png";
+
+const NavbarItem = ({ title, classProps }) => {
+  return <li className={`mx-4 cursor-pointer ${classProps}`}>{title}</li>;
+};
+
+const Navbar = () => {
+  return (
+    <nav className="w-full flex md:justify-center justify-between align-center p-4">
+      <div className="md:flex[0.5] flex-initial justify-center items-center">
+        <img src={logo} alt="logo" className="w-32 cursor-pointer" />
+      </div>
+      <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
+        {["Market", "Exchange", "Tutorial", "Wallet"].map((item, index) => (
+          <NavbarItem key={item + index} title={item} />
+        ))}
+        <li className="bg-[#2952e3]">Login</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
